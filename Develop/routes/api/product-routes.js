00d606da7191482e186d9fc196cbeddcc6ec66fd
Jordan Ring-Sakabe <jordanringsakabe@gmail.com>
2022-09-13 09:31:12 +0900

    added delete method for the product routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -102,8 +102,21 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where : {
+        id: req.params.id
+      }
+    });
+    if(!prdocutData) {
+      res.status(404).json({message: "No product found with this id number."})
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;
