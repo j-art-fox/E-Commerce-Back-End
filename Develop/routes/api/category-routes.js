7f3267a44db6b5da20ee14e0538330870622dc32
Jordan Ring-Sakabe <jordanringsakabe@gmail.com>
2022-09-13 13:26:11 +0900

    added routes for tags

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -13,7 +13,7 @@ router.get('/', async (req, res) => {
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
-    console.log("oops.")
+    console.log(err)
   }
 });
 
